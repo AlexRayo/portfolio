34dbd5ebb9ae5aed5507ff676ece8e1c418cbc4d
alexrayo <alexrayo.net@gmail.com>
2022-11-16 19:42:29 -0600

    first commit

---
+++ .next/server/pages/404.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/404";
+exports.ids = ["pages/404"];
+exports.modules = {
+
+/***/ "./src/pages/404.js":
+/*!**************************!*\
+  !*** ./src/pages/404.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction Page404() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n        children: \"Page not found!\"\n    }, void 0, false, {\n        fileName: \"C:\\\\Users\\\\Heisenberg\\\\Documents\\\\portfolio\\\\src\\\\pages\\\\404.js\",\n        lineNumber: 2,\n        columnNumber: 12\n    }, this);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Page404);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvcGFnZXMvNDA0LmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFBO0FBQUEsU0FBU0EsT0FBTyxHQUFHO0lBQ2YscUJBQU8sOERBQUNDLEtBQUc7a0JBQUMsaUJBQWU7Ozs7O1lBQU07QUFDbkMsQ0FBQztBQUVILGlFQUFlRCxPQUFPIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcG9ydGZvbGlvLy4vc3JjL3BhZ2VzLzQwNC5qcz83NTg3Il0sInNvdXJjZXNDb250ZW50IjpbImZ1bmN0aW9uIFBhZ2U0MDQoKSB7XHJcbiAgICByZXR1cm4gPGRpdj5QYWdlIG5vdCBmb3VuZCE8L2Rpdj5cclxuICB9XHJcbiAgXHJcbmV4cG9ydCBkZWZhdWx0IFBhZ2U0MDQgICJdLCJuYW1lcyI6WyJQYWdlNDA0IiwiZGl2Il0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/pages/404.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./src/pages/404.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+function HomePage() {
+    return <div>Welcome to Next.js DUDE!</div>
+  }
+  
+export default HomePage  
